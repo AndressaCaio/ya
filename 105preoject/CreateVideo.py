280e108b92d48c547b6834a75a328ef2ab7c4579
AndressaCaio <102971867+AndressaCaio@users.noreply.github.com>
2022-07-29 09:11:53 -0300

    Add files via upload

---
+++ 105preoject/CreateVideo.py
@@ -0,0 +1,15 @@
+import os
+import cv2
+
+path = "images/"
+list = images[]
+if ext in ['.gif','.png','.jpg','jpeg','jfif']:
+     file.name = path+"/"+file
+print(file_name)
+images.append('111.jpg','112.jpg','113.jpg','114,jpg','115.jpg','116.jpg','117.jpg','118.jpg','119.jpg','120.jpg')
+count = len
+images. frame = cv2.imread(imagens[0])
+frame.shape
+size = (width,height)
+print(size)
+out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'),0,8, size)
